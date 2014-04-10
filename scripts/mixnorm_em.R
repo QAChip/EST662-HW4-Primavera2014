6b4b17b670c73bc786d71c4562447869e68719f7
Christian I. Ponce <christian.ponce@foodgeek.pro>
2014-04-10 18:14:04 -0500

    +.gitignore incluye los folders figures/ y cache/, así como los temporales +Modificaciones al script mixnorm_em.R +Creación del archivo principal .Rnw con su estructura inicial, +Creación de archivos para la primera pregunta terminada, +Primera pregunta terminada sin revisión.

--- scripts/mixnorm_em.R
+++ scripts/mixnorm_em.R
@@ -21,7 +21,7 @@ em.mixnorm=function(x,n){
      X[i,]=c(pr,m1,m2,s1,s2)
      teta<<-list(pr=pr,mu1=m1,mu2=m2,s1=s1,s2=s2)
    }# Cierra for(i in 1:n){
-   print(X)
+   xtable(X)
    return(teta)
  }#Cierra function(x,n)
 
@@ -35,23 +35,23 @@ dmixnorm=function(Teta,i){
     (1-Teta$pr)*(dnorm(i,Teta$mu2,Teta$s2))
 }
 
-# ---- Soporte ----
+# ---- Soporte.pi ----
 soporte=function(i,k){
-  #se puede selecciona 1 o 2 para i
+  #se puede selecciona 1 o 2 para i,
   #k es el número de puntos a evaluar
   seq( range(faithful[,i])[1],
              range(faithful[,i])[2],
              by=(range(faithful[,i])[2]-range(faithful[,i])[1])/k)
 }
 
-# ---- HisFaithful[,1] ----
+# ---- HistFaithful1 ----
 teta=em.mixnorm(faithful[,1],n=50)
-a=soporte(1,k=100)
+a=soporte(i=1,k=100)
 y=dmixnorm(Teta=teta,i=a)
 
-# ---- HistFaithful[,2] ----
+# ---- HistFaithful2 ----
 teta=em.mixnorm(faithful[,2],n=50)
-b=soporte(2,k=100)
+b=soporte(i=2,k=100)
 z=dmixnorm(Teta=teta,i=b)
 
 # ---- PlotFaithful ----
