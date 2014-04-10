d0baec969855e5b7b7be82502ae153b56803335c
Christian I. Ponce <christian.ponce@foodgeek.pro>
2014-04-10 12:31:31 -0500

    PI(), PI.anti(), PI2() y PI2.anti() ya estiman correctamente.

--- scripts/hat_pi.R
+++ scripts/hat_pi.R
@@ -29,15 +29,37 @@ plot(x,apply(x,2,g), type="l")
 
 #---- Pi ----
 PI=function(i){
-  4/((1+i)^2)
-  #6/((2-x)*(1+i)^2) #Dividiendo entre la densidad g(x)
+  #4/(1+i^2) Se desactivó en branch pi-test
+  6/((2-x)*(1+i^2)) # f(x)/g(x)
+}
+
+PI2=function(i){
+  24/((1+i^2)^2)*(2-i)
 }
 
 PI.anti=function(i,j){
   # la variable i \sim U(0,1)
   # la variable j=1-i \sim U(0,1)
-  ( (4/((1+i)^2))+(4/((1+j)^2)) )/2
+  ( (6/((2-i)*(1+i^2)))+
+    (6/((2-j)*(1+j^2)))
+  ) /2
+}
+
+PI2.anti=function(i,j){
+  # la variable i \sim U(0,1)
+  # la variable j=1-i \sim U(0,1)
+  ( 24/((1+i^2)^2)*(2-i)+
+      24/((1+j^2)^2)*(2-j)
+  ) /2
 }
 # ---- \teta{Pi} ----
-x=randx.anti(100000)
-Teta=PI.anti(i=x[,1],j=x[,2])
+x=randx(10000000)
+Teta=PI(i=x)
+Teta2=PI2(i=x)
+s2.teta=mean(Teta2)-mean(Teta)^2
+
+# ---- \teta{Pi}.anti ----
+x=randx.anti(10000000)
+Teta.anti=PI.anti(i=x[,1],j=x[,2])
+Teta2.anti=PI2.anti(i=x[,1],j=x[,2])
+s2.teta.anti=mean(Teta2.anti)-mean(Teta.anti)^2
