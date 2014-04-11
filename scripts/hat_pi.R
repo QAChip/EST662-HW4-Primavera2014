d9f6b67eb8961886e7fdff5d41f264b75bdf19cf
Christian I. Ponce <christian.ponce@foodgeek.pro>
2014-04-10 23:57:23 -0500

    Estimación de pi por muestreo de importancia listo, Estimación de pi mediante variables antitéticas, parcialmente listo. La varianza es incorrecta, la estimación es incorrecta. Modificación de algunas funciones en la sección de variables antitéticas.

--- scripts/hat_pi.R
+++ scripts/hat_pi.R
@@ -12,7 +12,7 @@ df=function(x){
 # ----Soporte ----
 x=matrix(ncol=1, seq(0,1,by=0.001) )
 
-# ---- Plotg(x)f(x) ----
+# ---- Plotgf ----
 plot(x,apply(x,2,df),type="l", col="blue",xlab=expression(x),
      ylim=c(0,4), xlim=c(0,1), ylab=expression(list(f(x), g(x))))
 lines(x,apply(x,2,dg), type="l", col="red")
@@ -20,17 +20,18 @@ text(0.8,3,expression(f(x)==frac(4,(1+x^2))) )
 text(0.8,1.2, expression(g(x)==frac(2*(2-x),3)), ylim=c(0,3.6))
 
 
-# ---- -x(x-4)/3=u ----
+# ---- randx ----
 #u \sim U(0,1)
 #Generador de va X por el método de inversión
 
 randx=function(n){
   u=runif(n)
   x1=2-sqrt(4-3*u)
-  #x2=(2+sqrt(4-3*u)) No se usa porque X2>1 y 0<X<1
+ #x2=(2+sqrt(4-3*u)) No se usa porque X2>1 y 0<X<1
   return(x1)
 }
 
+# ----randx.anti ----
 randx.anti=function(n){
   u=runif(n)
   x1=2-sqrt(4-3*u)
@@ -42,14 +43,15 @@ randx.anti=function(n){
 
 #---- Pi ----
 PI=function(i){
-  #4/(1+i^2) Se desactivó en branch pi-test
   6/((2-x)*(1+i^2)) # f(x)/g(x)
 }
 
+# ---- Pi2 ----
 PI2=function(i){
   24/((1+i^2)^2)*(2-i)
 }
 
+# ---- Pi.anti ----
 PI.anti=function(i,j){
   # la variable i \sim U(0,1)
   # la variable j=1-i \sim U(0,1)
@@ -58,6 +60,7 @@ PI.anti=function(i,j){
   ) /2
 }
 
+# ----Pi2.anti
 PI2.anti=function(i,j){
   # la variable i \sim U(0,1)
   # la variable j=1-i \sim U(0,1)
@@ -65,25 +68,37 @@ PI2.anti=function(i,j){
     24/((1+j^2)^2)*(2-j)
   ) /2
 }
+
+# ---- hat.sigma2 ----
+s2=function(Teta){
+  m=seq(1:length(Teta)) #Denominador
+  (cumsum(Teta-cumsum(Teta)/m)^2)/m
+}
+
+# ---- hat.sigma2.anti ----
+s2.anti=function(Teta){
+  m=seq(1:length(Teta)) #Denominador
+  (cumsum((Teta[,1]+Teta[,1])/2-cumsum((Teta[,1]+Teta[,2])2)/m)^2)/m
+}
+
 # ---- N muestra ----
-n=10000
+n=1000
 
 # ---- hat(Pi) ----
 x=randx(n)
 Teta=PI(i=x)
 
-# ---- hat(sigma2)----
-Teta2=PI2(i=x)
-s2.teta=mean(Teta2)-mean(Teta)^2
-
 # ---- En(Teta) ----
 Teta.n=cumsum(Teta) #Sucesión de sumas parciales
 m=seq(1:length(Teta.n)) #Denominador
 En.Teta=Teta.n/m #Sucesión E_n[teta] m=1,...,n
-
+s2.teta=s2(Teta)
+q=qnorm(0.975)*sqrt(s2.teta/m)
 # ---- Plot ----
 plot(m, En.Teta, type="l", ylim=c(3.11,3.19),
      ylab=expression(y==E[n](hat(pi))))
+lines(m, En.Teta+q, col="red")
+lines(m, En.Teta-q, col="red")
 abline(h=pi, col="yellow")
 
 # ---- hat(Pi.anti) ----
